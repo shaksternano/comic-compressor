084ab3903702273436fb510aef60771a51fc5b10
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-04-23 17:12:30 +0100

    build: include dependencies in jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "io.github.shaksternano.comiccompressor"
 version = "1.0.0"
+base.archivesName.set("comic-compressor")
 
 repositories {
     mavenCentral()
@@ -21,9 +24,34 @@ dependencies {
 }
 
 tasks {
+    jar {
+        enabled = false
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        mergeServiceFiles()
+        manifest {
+            attributes(
+                mapOf(
+                    "Main-Class" to "${project.group}.MainKt",
+                )
+            )
+        }
+        dependsOn(distTar, distZip)
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
     test {
         useJUnitPlatform()
     }
+}
+
+application {
+    mainClass.set("${project.group}.MainKt")
 }
 
 kotlin {
